df35ee6425975b0b8ea798047032677822ffbea4
MaddieKRoberts <madkroberts2309@gmail.com>
2025-08-03 22:08:31 -0400

    Update xmlscraper.py

--- xmlscraper.py
+++ xmlscraper.py
@@ -4,5 +4,15 @@ tree = ET.parse('rollcall949.xml')
 print(tree)
 
 root = tree.getroot()
-for bill in root.iter ('bill')
-	print (bill.text)
+
+for legis in root.findall("./vote-data/recorded-vote/"):  #grabs legislators name & vote
+	legisvote = legis.text+","
+	print (legisvote)
+	
+for desc in root.findall("./vote-metadata/vote-desc/"):   #should grab the bill description, doesnt, dont know why
+	description = desc.tag
+	print (description)
+	
+nf=open("RCH949.csv","w")   #doesnt creat csv, dont know why
+nf.write(legisvote)
+nf.close()	
