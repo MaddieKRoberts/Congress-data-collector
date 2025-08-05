0d776c6de13b2bcd00a1a9a2fd1b7252cd3543db
satchmojoe <josephoroberts@gmail.com>
2025-08-04 20:55:17 -0400

    Update xmlscraper.py
    working example

--- xmlscraper.py
+++ xmlscraper.py
@@ -11,6 +11,12 @@ legisvote = ""
 for recordedvote in root.findall("./vote-data/"):  #grabs legislators name
 	recordedvote = legis.text+","
 
+# star dads code
+for v in root.findall("./vote-data/recorded-vote"):
+	# these two lines do the exact same thing, because v is
+	#the first set of recorded votes, v[0] is the first legislator in the first recorded vote
+	print(v[0].text)
+	print(v.find('./legislator').text)
 	
 desc = root.find(".//vote-desc").text+","
 print (desc)
