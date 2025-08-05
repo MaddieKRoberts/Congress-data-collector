ff78eccb03b7c5fd432df1b98ac0e5d48ecf7940
MaddieKRoberts <madkroberts2309@gmail.com>
2025-08-04 21:09:31 -0400

    all files

--- xmlscraper.py
+++ xmlscraper.py
@@ -16,18 +16,12 @@ for v in root.findall("./vote-data/recorded-vote"):
 	legisvote = v[1].text+","+"/n"
 	critterandvote = legisname+legisvote
 
-# star dads code
-for v in root.findall("./vote-data/recorded-vote"):
-	# these two lines do the exact same thing, because v is
-	#the first set of recorded votes, v[0] is the first legislator in the first recorded vote
-	print(v[0].text)
-	print(v.find('./legislator').text)
 	
 desc = root.find(".//vote-desc").text+","
 print (desc)
 print (critterandvote)
 RCD = desc+critterandvote
 
-nf=open("testfour.csv","w")   #doesnt creat csv, dont know why
+nf=open("testfive.csv","w")   #doesnt creat csv, dont know why
 nf.write(RCD)
 nf.close()	
