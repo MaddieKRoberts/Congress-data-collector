783436f84cd9dd634a5103332d4d8a56df1d0041
Maddie Kol Roberts <madkroberts2309@gmail.com>
2025-08-06 15:35:44 +0000

    sdfsdcs

--- congressdatabase.py
+++ congressdatabase.py
@@ -1,4 +1,13 @@
 import sqlite3
 
+#creates connection to "congress.db"
 conn = sqlite3.connect('congress.db')
 
+#creates a cursor connected to "congress.db"
+c = conn.cursor()
+
+#creates a table in congress
+c.execute("""CREATE TABLE critters (
+        name DATATYPE,
+        vote DATATYPE,
+    )""")
