08ab67cfa4ff599cd77b83ef8f6fcb5b6cd32c33
Maddie Kol Roberts <madkroberts2309@gmail.com>
2025-08-07 00:45:17 +0000

    changed ofr loop

--- congressrecordsapp.py
+++ congressrecordsapp.py
@@ -6,11 +6,10 @@ congdatafunc.emergencydeletetable()
 r = 1990
 n = 1
 t = 1200
-for i in range(r):
-    year = r
-    for z in range(t):
-        num = n
-        congdatafunc.addhousebill(year, num)
+for i in range(36):
+    
+    for z in range(1200):
+        congdatafunc.addhousebill(r, n)
         num+1
         time.sleep(10)
-    year+1
+    r+1
